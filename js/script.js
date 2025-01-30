bf9552aaa11ab807cbadaa922a7216ddb9ac6841
AyoSynk <161512257+synkfr@users.noreply.github.com>
2025-01-30 18:53:44 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,3 +13,18 @@ function stickyHeader() {
 window.onscroll = function() {
     stickyHeader();
 };
+
+
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
+const themeIcon = document.querySelector('.theme-icon');
+
+themeToggle.addEventListener('click', () => {
+  if (body.getAttribute('data-theme') === 'dark') {
+    body.removeAttribute('data-theme');
+    themeIcon.textContent = '⏾';
+  } else {
+    body.setAttribute('data-theme', 'dark');
+    themeIcon.textContent = ' ☀︎ ';
+  }
+});
